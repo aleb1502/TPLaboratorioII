337cbb1ccb6abde0b51ce94293fd69a7f8f89598
aleb1502 <31104436+aleb1502@users.noreply.github.com>
2017-08-17 22:00:20 -0300

    Proyecto 4 basico

---
+++ Ejercicio04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoEj4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num6, i;
+
+            Console.WriteLine("Ingrese el numero: ");
+            int.TryParse(Console.ReadLine(), out num);
+            int.TryParse(Console.ReadLine(), out num6);
+
+            if (num % 6 == 0 && num <= 6 * 4)
+            {
+                num6 = num;
+                num6++;
+            }
+
+            for (i = 0; i <= 6 * 4; i++)
+            {
+                Console.WriteLine("{0}", num6);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
